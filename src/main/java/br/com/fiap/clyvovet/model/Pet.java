1238609c461c5a38720e67546c22225ede5bfe7b
LucasMatsubara <lucasmatsubara11@gmail.com>
2026-05-17 23:55:12 -0300

    Adicionando tipagem para os lembretes.

--- src/main/java/br/com/fiap/clyvovet/model/Pet.java
+++ src/main/java/br/com/fiap/clyvovet/model/Pet.java
@@ -1,11 +1,15 @@
 package br.com.fiap.clyvovet.model;
 
+import br.com.fiap.clyvovet.enums.Especie;
+import br.com.fiap.clyvovet.enums.FaseVida;
+import br.com.fiap.clyvovet.enums.Porte;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_pets")
@@ -23,6 +27,18 @@ public class Pet {
     @Column(nullable = false, length = 100)
     private String raca;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Especie especie;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private FaseVida faseVida;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Porte porte;
+
     @Column(length = 255)
     private String descricao;
 
@@ -37,4 +53,7 @@ public class Pet {
     @ToString.Exclude
     private Tutor tutor;
 
+    @OneToMany(mappedBy = "pet", cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<PlanoTratamento> tratamentos;
 }
